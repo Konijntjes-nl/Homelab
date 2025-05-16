49fb4e43bae1e9c0360358d293e118b8626a324e
Mark Lam <mark.lam@live.nl>
2025-05-17 00:14:20 +0200

    First upload

---
+++ CyberArk/Scripts/HTML5.ps1
@@ -0,0 +1,32 @@
+﻿# Define your CyberArk PVWA details
+$PVWAURL = "https://pvwa.cybermark.lab/PasswordVault"
+$Username = "admin2"
+$Password = "LVxY7IQxlVLvtc8GzZ26EChqok1Ttxg3"  # Use secure method in production!
+$AuthType = "CyberArk"  # Or LDAP, Radius, etc.
+
+# Login to get token
+$body = @{
+    username = $Username
+    password = $Password
+    concurrentSession = "true"
+} | ConvertTo-Json
+
+$tokenResponse = Invoke-RestMethod -Uri "$PVWAURL/API/Auth/$AuthType/Logon" -Method POST -Body $body -ContentType "application/json"
+$token = $tokenResponse
+
+# Add token to header
+$headers = @{
+    Authorization = $token
+}
+
+# Get list of active sessions
+$sessions = Invoke-RestMethod -Uri "$PVWAURL/API/LiveSessions" -Headers $headers -Method GET
+
+# Filter for HTML5GW sessions
+$html5Sessions = $sessions.LiveSessions | Where-Object { $_.ConnectionComponentType -eq "PSM" }
+
+# Output results
+$html5Sessions | Select-Object UserName, Address, PlatformID, StartTime, ConnectionComponentType | Format-Table -AutoSize
+
+# Logoff to invalidate token
+Invoke-RestMethod -Uri "$PVWAURL/API/Auth/Logoff" -Headers $headers -Method POST
