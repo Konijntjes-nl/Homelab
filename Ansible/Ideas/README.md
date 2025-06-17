48922145b5c3f6d204ef3e46f30805727283060c
Mark Lam <mark.lam@live.nl>
2025-06-17 22:41:31 +0200

    fix

--- Ansible/Ideas/README.md
+++
@@ -1,6 +0,0 @@
-# Homelab
-
-1. Create a script to automate VM creation
-    1. Windows
-    2. Linux 
-2. 
--- CyberArk/Scripts/Staging/Monitoring/4. Send-EmailAlert.ps1
+++
@@ -1,9 +0,0 @@
-# Send-EmailAlert.ps1
-param (
-    [string]$Subject = "CyberArk Alert",
-    [string]$Body = "A CyberArk monitoring threshold was triggered.",
-    [string]$To = "admin@company.com"
-)
-
-Send-MailMessage -To $To -From "cyberark-monitor@company.com" `
-    -Subject $Subject -Body $Body -SmtpServer "smtp.company.com"
